7050f7f0d6a55b49f3a795e541070521b1c5cc53
Andy Tran <rooneytai5236@gmail.com>
2024-10-07 02:12:43 +1100

    End of section 9

--- src/IdentityService/Config.cs
+++ src/IdentityService/Config.cs
@@ -40,6 +40,7 @@ public static class Config
                 AllowOfflineAccess = true,
                 AllowedScopes = {"openid", "profile", "auctionApp"},
                 AccessTokenLifetime = 3600*24*30,
+                AlwaysIncludeUserClaimsInIdToken = true,
             }
         };
 }
