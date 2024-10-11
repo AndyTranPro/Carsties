e63daf2fcdaf76e965a72c3d27b80304cca3621a
Andy Tran <rooneytai5236@gmail.com>
2024-10-11 17:53:42 +1100

    End of section 12

---
+++ src/NotificationService/Program.cs
@@ -0,0 +1,31 @@
+using MassTransit;
+using NotificationService.Consumers;
+using NotificationService.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumersFromNamespaceContaining<AuctionCreatedConsumer>();
+
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("nt", false));
+
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.Host(builder.Configuration["RabbitMq:Host"], "/", h =>
+        {
+            h.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+            h.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+        });
+
+        cfg.ConfigureEndpoints(context);
+    });
+});
+
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+app.MapHub<NotificationHub>("/notifications");
+
+app.Run();
